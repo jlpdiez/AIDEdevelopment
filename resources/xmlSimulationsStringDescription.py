c13557f5e1ebe1df57ec4822079bc9d613f48de5
Juan Luis Perez Diez <jlpdiez@ucm.es>
2018-10-03 14:05:55 +0200

    Add script to get SimNames and descriptions

---
+++ resources/xmlSimulationsStringDescription.py
@@ -0,0 +1,22 @@
+import xml.etree.ElementTree as ET
+
+data = list()
+for i in range(10):
+    print(i+1)
+    data.append("/home/ender/Desktop/AIDEdevelopment/e" + str(i + 1) + "interview/src/main/spec/specification.xml")
+
+    tree = ET.parse(data[i])
+    root = tree.getroot()
+    keys = root.findall("./models/model/object")
+
+    for key in keys:
+        if (key.attrib["type"] == "ingenias.editor.entities.SimulationDiagramDataEntity"):
+            print("true")
+            simDescs = key.findall("./mapproperties/key")
+            for desc in simDescs:
+                if (desc.attrib["id"] == "Description"):
+                    if ((desc.text != "Yes") & 
+                        (desc.text != "INGENIAS") & 
+                        (desc.text is not None)):
+                        print(desc.text)
+        
