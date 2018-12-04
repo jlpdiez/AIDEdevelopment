7e8cd6a1f505fa5d5f3de79e11f6f461d3074bef
Juan Luis Perez Diez <jlpdiez@ucm.es>
2018-12-04 13:15:42 +0100

    Update script to generate sites + Excluded folder from gitignore

--- resources/generateSites.py
+++ resources/generateSites.py
@@ -7,4 +7,7 @@ dir_list = [f for f in rootdir.glob('**/*interview') if f.is_dir()]
 print(dir_list)
 #Execute "mvn site" command in each of the folders
 for d in dir_list:
-	subprocess.Popen("mvn site", cwd=str(d), shell=True)
+	#Non-blocking call
+	#subprocess.Popen("mvn site", cwd=str(d), shell=True)
+	#blocking call
+	subprocess.call("mvn site", cwd=str(d), shell=True)
